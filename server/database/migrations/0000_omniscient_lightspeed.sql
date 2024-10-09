11945ff6d39587148e1c2c27aceb11f93a3bc0ec
Rihan Arfan <me@file.properties>
2024-10-09 18:56:10 +0100

    feat: add full text search table and set cascade delete

--- server/database/migrations/0000_omniscient_lightspeed.sql
+++ server/database/migrations/0000_omniscient_lightspeed.sql
@@ -3,7 +3,7 @@ CREATE TABLE `document_chunks` (
 	`document_id` text,
 	`text` text,
 	`session_id` text,
-	FOREIGN KEY (`document_id`) REFERENCES `documents`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`document_id`) REFERENCES `documents`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE TABLE `documents` (
@@ -14,3 +14,25 @@ CREATE TABLE `documents` (
 	`session_id` text,
 	`r2_url` text
 );
+--> statement-breakpoint
+CREATE VIRTUAL TABLE document_chunks_fts USING fts5(
+	id UNINDEXED,
+	document_id UNINDEXED,
+	text,
+	session_id UNINDEXED,
+	content = 'document_chunks'
+);
+
+CREATE TRIGGER document_chunks_ai
+AFTER INSERT
+	ON document_chunks BEGIN
+INSERT INTO
+	document_chunks_fts(id, document_id, text, session_id)
+VALUES
+	(
+		new.id,
+		new.document_id,
+		new.text,
+		new.session_id
+	);
+END;
--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,7 +1,7 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 
 export const documents = sqliteTable('documents', {
-  id: text('id').primaryKey(),
+  id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
   name: text('name'),
   textContent: text('text_content'),
   size: integer('size'),
@@ -10,8 +10,8 @@ export const documents = sqliteTable('documents', {
 })
 
 export const documentChunks = sqliteTable('document_chunks', {
-  id: text('id').primaryKey(),
-  documentId: text('document_id').references(() => documents.id),
+  id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+  documentId: text('document_id').references(() => documents.id, { onDelete: 'cascade' }),
   text: text('text'),
   sessionId: text('session_id'),
 })
