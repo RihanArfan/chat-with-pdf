bb0a0bfb7500bb56748432e71b18ed18dfa22d52
Rihan Arfan <me@file.properties>
2024-10-13 22:45:40 +0100

    feat: :mag: add meta info

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: 'bg-white dark:bg-zinc-900 overscroll-none',
+  },
+})
+useSeoMeta({
+  title: 'Chat with PDF',
+  ogTitle: 'Chat with your PDF documents',
+  description:
+    'Chat with PDF lets you ask questions to PDFs. Built using NuxtHub and Nuxt UI.',
+  ogImage: '', // TODO:
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator />
